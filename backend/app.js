ab565331295e300f27fcdfe19905bb077ed0331e
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-05-27 12:02:36 +0200

    image upload to tmp works

--- backend/app.js
+++ backend/app.js
@@ -22,6 +22,37 @@ app.use(cors({
     origin: true
 }));
 
+import multer from "multer";
+
+app.use("/upload", express.urlencoded({extended: true}));
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+      callback(null, './tmp/uploads/');
+    },
+    filename: function (req, file, callback) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        callback(null, uniqueSuffix + file.originalname.substring(file.originalname.lastIndexOf(".")))
+    }
+  });
+
+const upload = multer({storage: storage});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    if (!req.file) {
+      console.log("No file received");
+      return res.send({
+        success: false
+      });
+  
+    } else {
+      console.log('file received', req.file.filename);
+      return res.send({
+        success: true
+      })
+    }
+  });
+
 import userRouter from "./routes/userRouter.js";
 app.use(userRouter);
 
