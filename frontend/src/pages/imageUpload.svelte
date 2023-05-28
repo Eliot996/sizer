78d7df548b7a036b8be8338ff0f20923f9bdace8
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-05-28 11:15:32 +0200

    multi image upload rough

--- frontend/src/pages/imageUpload.svelte
+++ frontend/src/pages/imageUpload.svelte
@@ -3,7 +3,9 @@
 
     function send() {
         const fd = new FormData();
-        fd.append("file", imageInput.files[0]);
+        queuedImagesArray.forEach(file => {
+            fd.append("files", file);
+        })
 
         fetch($BASE_URL + "/upload", {
             method: "POST",
@@ -11,12 +13,77 @@
         })
     }
 
+    let queuedImagesArray = [];
+    let imageInput;
+    const savedForm = document.getElementById("saved-form");
+    const queuedForm = document.getElementById("queued-form");
+    const savedDiv = document.getElementsByClassName("saved-div");
+    const queuedDiv = document.getElementsByClassName("queued-div");
+    const inpuDiv = document.getElementsByClassName("input-div");
+    const serverMessage = document.getElementsByClassName(".server-message");
+
+    // Queued in frontend images
+
+    $: {
+        const files = imageInput;
+        if (files) {
+            for (let i = 0; i < files.length; i++) {
+                queuedImagesArray = [ ...queuedImagesArray, files[i] ];
+            }
+            imageInput = null;
+        }      
+    }
+
+    function inputDrop(event) {
+        event.preventDefault();
+        const files = event.dataTransfer.files;
+        for (let i = 0; i < files.length; i++) {
+            if (!files[i].type.match("image")) continue
+            
+            if (queuedImagesArray.every(image => image.name !== files[i].name)) {
+                queuedImagesArray = [ ...queuedImagesArray, files[i] ];
+            }
+        }
+    }
+
+    function deleteQueuedImage(index) {
+        queuedImagesArray = [...queuedImagesArray.slice(0, index), ...queuedImagesArray.slice(index + 1)]
+    }
 </script>
 
+<div class="header">
+    <h2>Image upload</h2>
+    <div class="server-messages"></div>
+</div>
 
-<h1>Image upload</h1>
+<div class="input-div" on:drop={inputDrop}>
+    <p>Drag and drop images here or <span class="browse">browse</span></p>
+    <input bind:files={imageInput} type="file" accept="image/png, image/jpg, image/jpeg" multiple>
+</div>
 
-<input id="image-input" name="image" type="file" accept="image/png, image/jpg, image/jpeg">
-<label for="image"></label>
+<form id="saved-form">
+    <div class="header">
+        <h2>Saved In server</h2>
+        <button type="submit">Delete</button>
+    </div>
+    <div class="saved-div"></div>
+</form>
+
+<form id="queued-form">
+    <div class="header">
+        <h3>Queued in Frontend</h3>
+        <button type="submit">Upload</button>
+    </div>
+    <div class="queued-div">
+        {#each  queuedImagesArray as image, index }
+            <div class="image">
+                <img src="{URL.createObjectURL(image)}" alt="{image.name}">
+                <span on:click={() => deleteQueuedImage(index)}>&times;</span>
+            </div>
+            <div>{queuedImagesArray}</div>
+        {/each}
+    </div>
+</form>
 
 <button class="button" on:click="{send}">Send</button>
+
