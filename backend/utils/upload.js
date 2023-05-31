a42d0729b6b24c106be2370bf0676b91b6c92773
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-05-31 13:58:58 +0200

    upload handling with multer

---
+++ backend/utils/upload.js
@@ -0,0 +1,35 @@
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+// making sure the tmp folders are made 
+fs.mkdir(process.cwd() + "/tmp/uploads", {recursive: true}, () => {});
+fs.mkdir(process.cwd() + "/tmp/downloads", {recursive: true}, () => {});
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, './tmp/uploads/');
+    },
+    filename: function (req, file, callback) {
+        const uniquePrefix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        callback(null, uniquePrefix + path.extname(file.originalname));
+    }
+});
+
+const fileFilter = (req, file, callback) => {
+    const fileTypes = /png|jpg|jpeg/;
+    const fileExtention = path.extname(file.originalname).toLowerCase();
+
+    const extname = fileTypes.test(fileExtention);
+    const mimetype = fileTypes.test(file.mimetype);
+    
+    if (extname && mimetype) {
+        callback(null, true);
+    } else {
+        callback("Please only upload images", false);
+    }
+}
+
+const upload = multer({storage, fileFilter});
+
+export default upload ;
