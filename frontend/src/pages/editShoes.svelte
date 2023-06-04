0d4005e90867aeabf30c2b94df277fa711503953
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-06-04 22:29:32 +0200

    added toasts

--- frontend/src/pages/editShoes.svelte
+++ frontend/src/pages/editShoes.svelte
@@ -2,6 +2,7 @@
     import SecretGuard from "./secretGuard.svelte";
     import { BASE_URL } from "../store/globalsStore.js";
     import ImageManagement from "../components/imageUpload/imageManagement.svelte";
+    import toast, { Toaster } from 'svelte-french-toast';
 
     let sendImages;
 
@@ -9,14 +10,23 @@
     let socket = io($BASE_URL);
 
     export let brand, name, size;
+
+    function save() {
+        toast.promise(sendImages(), {
+                    loading: 'Saving changes',
+                    success: 'Changes saved',
+                    error: 'Could not save, try again later',
+            });
+    }
 </script>
 
     <SecretGuard>
+        <Toaster />
 
         <h1>{brand} {name}</h1>
         <h3>{size}</h3>
 
-        <button on:click={sendImages}>Save</button>
+        <button on:click={save}>Save</button>
 
         <ImageManagement target={`${$BASE_URL}/shoes/${brand}/${name}/${size}`} bind:socket={socket} bind:send={sendImages}/>
 
